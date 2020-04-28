dea67825f5ace6e8b9e1867cd455f9cb7dbb83df
luckyaxl <luckyaxl3@gmail.com>
2020-04-28 21:27:31 +0700

    ROCK ON

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
+import SignIn from "./modals/SignIn";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import konsul from "../assets/img/konsultasi.svg";
 import covid from "../assets/headerimg/covid.svg";
@@ -9,9 +10,34 @@ import rumah from "../assets/headerimg/rumah.svg";
 import tangan from "../assets/headerimg/tangan.svg";
 
 class Header extends Component {
+  state = {
+    auth: false,
+    open: false,
+    redir: false
+  };
+
+  componentDidMount() {
+    const auth = localStorage.getItem("auth");
+    this.setState({ auth });
+  }
+
+  handleClick = () => {
+    if (this.state.auth) {
+      this.setState({ redir: true });
+    } else {
+      this.setState({ open: !this.state.open });
+    }
+  };
+
   render() {
+    if (this.state.redir) return <Redirect to="/reservation" />;
+
     return (
       <div style={{ marginTop: 50 }}>
+        <SignIn
+          show={this.state.open}
+          onHide={() => this.setState({ open: !this.state.open })}
+        />
         <Jumbotron>
           <div className="container py-5 d-flex align-items-top justify-content-between">
             <div>
@@ -23,28 +49,49 @@ class Header extends Component {
                 </div>
               </div>
               <p className="mt-3" style={{ marginLeft: 0 }}>
-                <Link to="/reservation">
-                  <button
-                    className="btn btn-light"
-                    style={{ color: "#ff6185" }}
-                  >
-                    <img
-                      style={{ height: 40 }}
-                      className="mr-3"
-                      alt=".."
-                      src={konsul}
-                    />
-                    Konsultasi Dengan Dokter
-                  </button>
-                </Link>
+                <button
+                  onClick={this.handleClick}
+                  className="btn btn-light"
+                  style={{ color: "#ff6185" }}
+                >
+                  <img
+                    style={{ height: 40 }}
+                    className="mr-3"
+                    alt=".."
+                    src={konsul}
+                  />
+                  Konsultasi Dengan Dokter
+                </button>
               </p>
             </div>
-            <div className="d-flex">
-              <img src={ramai} alt="..." className="mr-4" />
-              <img src={tangan} alt="..." className="mr-4" />
-              <img src={mata} alt="..." className="mr-4" />
-              <img src={rumah} alt="..." className="mr-4" />
+            <div className="d-flex" style={{ maxWidth: "620px" }}>
+              <div className="text-center">
+                <img src={ramai} alt="..." className="" />
+                <p className="bold text-white mt-3">
+                  Tidak mendatangi tempat ramai
+                </p>
+              </div>
 
+              <div className="text-center">
+                <img src={tangan} alt="..." className="" />
+                <p className="bold text-white mt-3">
+                  Hindari untuk berjabat tangan
+                </p>
+              </div>
+
+              <div className="text-center">
+                <img src={mata} alt="..." className="" />
+                <p className="bold text-white mt-3">
+                  Hindari menyentuh mata secara langsung
+                </p>
+              </div>
+
+              <div className="text-center">
+                <img src={rumah} alt="..." className="" />
+                <p className="bold text-white mt-3">
+                  Tetap dirumah sampai pandemi selesai
+                </p>
+              </div>
             </div>
           </div>
         </Jumbotron>
