8cc5abb85de5afcab67428d0352fb025ddaf0424
luckyaxl <luckyaxl3@gmail.com>
2020-04-28 16:43:44 +0700

    add cons page

--- frontend/src/components/Consultation.js
+++ frontend/src/components/Consultation.js
@@ -5,37 +5,38 @@ class Consultation extends Component {
   render() {
     const { data: consul, success, loading, error } = this.props.consul;
 
-    console.log(consul)
+    console.log(consul);
 
     return (
       <>
-        {consul &&(
-            <div className="card p-3">
-              <div className="d-flex align-items-top mb-3">
-                <div className="mr-3">
-                  <img
-                    src="https://www.eastriseaviation.com/wp-content/uploads/2015/11/blank-profile-picture-200x200.png"
-                    alt="..."
-                    className="avatar"
-                    style={{ height: 60, width: 60, borderRadius: 45 }}
-                  />
+        {consul && (
+          <div className="card p-3">
+            <div className="d-flex align-items-top mb-3">
+              <div className="mr-3">
+                <img
+                  src="https://www.eastriseaviation.com/wp-content/uploads/2015/11/blank-profile-picture-200x200.png"
+                  alt="..."
+                  className="avatar"
+                  style={{ height: 60, width: 60, borderRadius: 45 }}
+                />
+              </div>
+              <div className="d-flex w-100 justify-content-between align-content-top">
+                <div>
+                  <h5 className="mb-0">{consul.subject}</h5>
+                  <small>16 April 2020</small>
+                  <br />
+                  <small>{consul.description}</small>
                 </div>
-                <div className="d-flex w-100 justify-content-between align-content-top">
-                  <div>
-                    <h5 className="mb-0">{consul.subject}</h5>
-                    <small>16 April 2020</small>
-                    <br />
-                    <small>{consul.description}</small>
-                  </div>
-                  <div>
-                    <small className="bold">17 April 2020</small>
-                  </div>
+                <div>
+                  <small className="bold">17 April 2020</small>
                 </div>
               </div>
-              <div className="border-top">
-                <div className="text-center mt-3">
-                  <h5>Waiting For Reply</h5>
-                </div>
+            </div>
+            <div className="border-top">
+              <div className="text-center mt-3">
+                <h5>Waiting For Reply</h5>
+              </div>
+              {/**
 
                 <div
                   style={{ marginLeft: 70 }}
@@ -57,9 +58,11 @@ class Consultation extends Component {
                     </div>
                   </div>
                 </div>
-              </div>
+
+                 */}
             </div>
-          )}
+          </div>
+        )}
       </>
     );
   }
