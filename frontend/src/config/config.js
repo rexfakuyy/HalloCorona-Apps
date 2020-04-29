ee42fe099b53a0e75a1b0a4442b7431236b728bb
Luckyaxl <luckyaxl3@gmail.com>
2020-04-29 14:53:29 +0700

    Delete config.js

--- frontend/src/config/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    siteTitle: "Notifday.com",
-    siteTitleShort: "greensend",
-    siteTitleAlt: "greensend",
-    themeColor: "#000000",
-  };
-  
-  module.exports = config;
-  
